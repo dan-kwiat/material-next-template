0e559245962a707e21324cb0515a552c66cf1b15
dan-kwiat <dan.kwiat@gmail.com>
2018-12-07 08:47:05 +0000

    Control list item selected state

--- components/Drawer.js
+++ components/Drawer.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import PropTypes from 'prop-types'
 import NavDrawer, {
   DrawerHeader,
@@ -13,29 +14,45 @@ import List, {
 } from '@material/react-list'
 import MaterialIcon from '@material/react-material-icon'
 
-const DrawerList = ({ onItemClick }) => (
-  <List>
-    <ListItem onClick={onItemClick}>
-      <ListItemGraphic graphic={<MaterialIcon icon='home'/>} />
-      <ListItemText primaryText='Home' />
-    </ListItem>
-    <ListItem onClick={onItemClick}>
-      <ListItemGraphic graphic={<MaterialIcon icon='search'/>} />
-      <ListItemText primaryText='Search' />
-    </ListItem>
-    <ListItem onClick={onItemClick}>
-      <ListItemGraphic graphic={<MaterialIcon icon='settings'/>} />
-      <ListItemText primaryText='Settings' />
-      <ListItemMeta meta='meta info' />
-    </ListItem>
-  </List>
-)
+class DrawerList extends React.Component {
+  state = {
+    selectedIndex: 0,
+  }
+  setSelectedIndex = selectedIndex => {
+    console.log(`Selected index: ${selectedIndex}`)
+    this.setState({ selectedIndex })
+  }
+  render() {
+    const { onItemClick } = this.props
+    return (
+      <List
+        singleSelection
+        selectedIndex={this.state.selectedIndex}
+        handleSelect={this.setSelectedIndex}
+      >
+        <ListItem onClick={onItemClick}>
+          <ListItemGraphic graphic={<MaterialIcon icon='home'/>} />
+          <ListItemText primaryText='Home' />
+        </ListItem>
+        <ListItem onClick={onItemClick}>
+          <ListItemGraphic graphic={<MaterialIcon icon='search'/>} />
+          <ListItemText primaryText='Search' />
+        </ListItem>
+        <ListItem onClick={onItemClick}>
+          <ListItemGraphic graphic={<MaterialIcon icon='settings'/>} />
+          <ListItemText primaryText='Settings' />
+          <ListItemMeta meta='meta info' />
+        </ListItem>
+      </List>
+    )
+  }
+}
 DrawerList.propTypes = {
   onItemClick: PropTypes.func.isRequired,
 }
 
 const DeviceAppropriateDrawer = ({ isOpen, setIsDrawerOpen, children }) => (
-  <>
+  <React.Fragment>
     <NavDrawer
       className='permanent-drawer'
     >
@@ -62,7 +79,7 @@ const DeviceAppropriateDrawer = ({ isOpen, setIsDrawerOpen, children }) => (
         }
       }
     `}</style>
-  </>
+  </React.Fragment>
 )
 DeviceAppropriateDrawer.propTypes = {
   isOpen: PropTypes.bool.isRequired,
@@ -85,7 +102,7 @@ const Drawer = props => (
           props.setIsDrawerOpen && props.setIsDrawerOpen(false)
         }}
       />
-      <a href='/'>test link</a>
+      <a href='/'>plain link</a>
     </DrawerContent>
   </DeviceAppropriateDrawer>
 )
