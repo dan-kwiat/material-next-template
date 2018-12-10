7b70dbfd069f85187085042c930c06a2435fe260
dan-kwiat <dan.kwiat@gmail.com>
2018-12-10 07:54:22 +0000

    Add explicit ListItem click handler

--- components/Drawer/DrawerList.js
+++ components/Drawer/DrawerList.js
@@ -13,26 +13,30 @@ class DrawerList extends React.Component {
     selectedIndex: 0,
   }
   setSelectedIndex = selectedIndex => {
-    console.log(`Selected index: ${selectedIndex}`)
+    console.log(`List.handleSelect index: ${selectedIndex}`)
     this.setState({ selectedIndex })
   }
+  handleClick = selectedIndex => () => {
+    console.log(`ListItem.onClick index: ${selectedIndex}`)
+    this.setState({ selectedIndex })
+    this.props.onItemClick()
+  }
   render() {
-    const { onItemClick } = this.props
     return (
       <List
         singleSelection
         selectedIndex={this.state.selectedIndex}
         handleSelect={this.setSelectedIndex}
       >
-        <ListItem onClick={onItemClick}>
+        <ListItem onClick={this.handleClick(0)}>
           <ListItemGraphic graphic={<MaterialIcon icon='home'/>} />
           <ListItemText primaryText='Home' />
         </ListItem>
-        <ListItem onClick={onItemClick}>
+        <ListItem onClick={this.handleClick(1)}>
           <ListItemGraphic graphic={<MaterialIcon icon='search'/>} />
           <ListItemText primaryText='Search' />
         </ListItem>
-        <ListItem onClick={onItemClick}>
+        <ListItem onClick={this.handleClick(2)}>
           <ListItemGraphic graphic={<MaterialIcon icon='settings'/>} />
           <ListItemText primaryText='Settings' />
           <ListItemMeta meta='meta info' />
